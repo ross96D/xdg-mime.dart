69bcd89f6a56c5aaf163931583d14e3c592020a2
ross96D <abelaross96@gmail.com>
2026-04-17 14:16:21 -0400

    work work wok

--- pkgs/desktop_entry/lib/desktop_entry.dart
+++ pkgs/desktop_entry/lib/desktop_entry.dart
@@ -3,20 +3,41 @@ library;
 import 'dart:io';
 
 import 'package:dartx/dartx_io.dart';
+import 'package:freedesktop_file_parser/parser.dart';
+import 'package:result/result.dart';
 import 'package:xdg_dir/xdg.dart';
-
+import 'package:path/path.dart' as p;
 import 'src/types.dart';
+
 export 'src/types.dart';
 
 class DesktopEntryManager {
-  List<({DesktopEntry entry, String path})> desktopEntries;
+  List<({DesktopEntry entry, String path})> _desktopEntries;
 
   static final List<Directory> _lookupDirs = [
     dataHome,
     ...dataDirs,
   ].map((e) => e.directory("applications")).toList();
 
-  DesktopEntryManager.create() : desktopEntries = [] {
+  DesktopEntryManager._(this._desktopEntries);
 
+  static Future<DesktopEntryManager> create() async {
+    final desktopEntries = <({DesktopEntry entry, String path})>[];
+
+    for (final dir in _lookupDirs) {
+      final dirEntries = dir.listSync();
+      for (final entry in dirEntries) {
+        if (entry is File && p.extension(entry.path) == ".desktop") {
+          switch (await DesktopEntry.fromFile(entry)) {
+            case ResultOk<DesktopEntry, ParseError>(ok: final desktopEntry):
+              desktopEntries.add((path: entry.absolute.path, entry: desktopEntry));
+            case ResultErr<DesktopEntry, ParseError>():
+              continue;
+          }
+        }
+      }
+    }
+
+    return DesktopEntryManager._(desktopEntries);
   }
 }
